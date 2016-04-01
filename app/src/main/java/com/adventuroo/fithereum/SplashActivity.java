a79f1fda371c54a804e5d398920e6f9093779575
Jeff Cailteux <jcailteux@fitbit.com>
2016-04-01 12:18:50 -0700

    chrome tab login progress

--- app/src/main/java/com/adventuroo/fithereum/SplashActivity.java
+++ app/src/main/java/com/adventuroo/fithereum/SplashActivity.java
@@ -1,18 +1,22 @@
 package com.adventuroo.fithereum;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by jcailteux on 4/1/16.
  */
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
+
 
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.a_splash);
+
+
+		startActivity(new Intent(this, LoginActivity.class));
+
 	}
-
-
 }
