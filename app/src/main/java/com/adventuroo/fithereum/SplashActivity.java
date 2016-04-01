28bd15d35d95622a7e230dab57276d22005d1358
Jeff Cailteux <jcailteux@fitbit.com>
2016-04-01 11:29:08 -0700

    splash activity

--- app/src/main/java/com/adventuroo/fithereum/SplashActivity.java
+++ app/src/main/java/com/adventuroo/fithereum/SplashActivity.java
@@ -1,7 +1,18 @@
 package com.adventuroo.fithereum;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by jcailteux on 4/1/16.
  */
-public class SplashActivity {
+public class SplashActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		setContentView(R.layout.a_splash);
+	}
+
+
 }
